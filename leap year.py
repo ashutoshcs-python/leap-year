047fdb50a8f30ffc6bcbc8b33a3aabd04ea86e68
ashutoshcs-python <72990939+ashutoshcs-python@users.noreply.github.com>
2020-10-16 22:33:14 +0530

    Update leap year.py
    Эта программа работает все годы

--- leap year.py
+++ leap year.py
@@ -1,6 +1,15 @@
-number = input("Введите год: ")
-number = int(number)
-if number % 4 == 0:
-   print("Это высокосный год")
-else:
-   print("Это не высокосный год")
+def leapyear(year):
+    leap = False
+    #Логическое тело
+    if year % 4 == 0:
+        leap = True
+        if year % 100 ==0 and year % 400!=0:
+                leap = False
+        elif year % 400:
+                leap = True
+    return leap
+#ввод и вывод соответственно
+year = int(input())
+print(leapyear(year))
+    
+
